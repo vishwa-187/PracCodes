38de2fa45376090ef2c9baddd68500273f97264f
Vishwa Shah <vishwashah180701@gmail.com>
2021-09-06 15:42:13 +0530

    27. Array -> Factorial

---
+++ 450/27. factorial.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    int kthElement(int arr1[], int arr2[], int n, int m, int k)
+    {
+        int fin[n+m];
+        for( int i =0;i<n;i++){
+            fin[i] = arr1[i];
+            
+        }
+        for( int j = 0;j<m;j++){
+            fin[n+j] = arr2[j]; 
+        }
+        sort(fin , fin + n+ m);
+        return fin[k-1];
+    }
+};
+
+// { Driver Code Starts.
+ 
+// Driver code
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--){
+		int n,m,k;
+		cin>>n>>m>>k;
+		int arr1[n],arr2[m];
+		for(int i=0;i<n;i++)
+			cin>>arr1[i];
+		for(int i=0;i<m;i++)
+			cin>>arr2[i];
+		
+		Solution ob;
+        cout << ob.kthElement(arr1, arr2, n, m, k)<<endl;
+	}
+    return 0;
+}  // } Driver Code Ends
